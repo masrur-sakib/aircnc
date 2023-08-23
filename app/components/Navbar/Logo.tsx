33c68e7a8d74b78a455f856e7284236d140241ce
masrur-sakib <mashrur333@gmail.com>
2023-08-23 08:09:16 +0600

    site root url added in the site logo.

--- app/components/Navbar/Logo.tsx
+++ app/components/Navbar/Logo.tsx
@@ -5,7 +5,10 @@ import { useRouter } from "next/navigation";
 const Logo = () => {
   const router = useRouter();
   return (
-    <p className="hidden md:block cursor-pointer text-green-400 text-2xl font-bold">
+    <p
+      onClick={() => router.push("/")}
+      className="hidden md:block cursor-pointer text-green-400 text-2xl font-bold"
+    >
       Aircnc
     </p>
   );
