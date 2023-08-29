5fbbc9f19ba8f71bd6fc65219e36e7262d619137
masrur-sakib <mashrur333@gmail.com>
2023-08-29 11:33:51 +0600

    made the map dynamic with country selection in dropdown.

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -28,10 +28,11 @@ const Map: React.FC<MapProps> = ({ center }) => {
       scrollWheelZoom={false}
       className="h-[35vh] rounded-lg"
     >
-        <TileLayer
-      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    />
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {center && <Marker position={center as L.LatLngExpression} />}
     </MapContainer>
   );
 };
--- app/components/Modals/RentModal.tsx
+++ app/components/Modals/RentModal.tsx
@@ -8,7 +8,8 @@ import { categories } from "../Navbar/Categories";
 import CategoryInput from "../inputs/CategoryInput";
 import { FieldValues, useForm } from "react-hook-form";
 import CountrySelect from "../inputs/CountrySelect";
-import Map from "../Map";
+import dynamic from "next/dynamic";
+// import Map from "../Map";
 
 enum STEPS {
   CATEGORY = 0,
@@ -46,6 +47,14 @@ const RentModal = () => {
 
   const category = watch("category");
   const location = watch("location");
+
+  const Map = useMemo(
+    () =>
+      dynamic(() => import("../Map"), {
+        ssr: false,
+      }),
+    [location]
+  );
 
   const setCustomValue = (id: string, value: any) => {
     setValue(id, value, {
@@ -120,7 +129,7 @@ const RentModal = () => {
           value={location}
           onChange={(value) => setCustomValue("location", value)}
         />
-        <Map />
+        <Map center={location?.latlng} />
       </div>
     );
   }
