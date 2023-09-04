2bbe4589e3571f03d748569e17ccf4f3f1a61593
masrur-sakib <mashrur333@gmail.com>
2023-09-04 23:58:28 +0600

    listing reservation calendar development - ongoing

---
+++ app/components/listings/ListingReservation.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+interface ListingReservationProps {
+  price: number;
+  dateRange: Range;
+  totalPrice: number;
+  onChangeDate: (value: Range) => void;
+  onSubmit: () => void;
+  disabled?: boolean;
+  disabledDates: Date[];
+}
+
+const ListingReservation = () => {
+  return <div></div>;
+};
+
+export default ListingReservation;
